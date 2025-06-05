48a8c031dc9077d723b1651714e734cb5a33c712
ajin <aj5210@naver.com>
2025-06-06 01:24:01 +0900

    Style: 프로젝트 전체 파일에 prettier 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "unsolved.ac - 친구들과 안 푼 백준 문제 찾기",
+  title: 'unsolved.ac - 친구들과 안 푼 백준 문제 찾기',
   description:
-    "여러 명이 아직 풀지 않은 백준 문제를 쉽게 비교하고, 원하는 조건으로 필터링해보세요.",
-};
+    '여러 명이 아직 풀지 않은 백준 문제를 쉽게 비교하고, 원하는 조건으로 필터링해보세요.',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="ko">
       <body>{children}</body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>unsolved.ac</div>;
+  return <div>unsolved.ac</div>
 }
