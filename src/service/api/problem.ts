b10aa20e55c155d6414be1017461a2945f78f640
ajin <aj5210@naver.com>
2025-06-06 21:41:02 +0900

    Refactor: Problem data 티어별 설정가능하게 수정
    - 티어(Silver, Gold, Platinum)
    - API 호출 제한으로 티어 분리

--- src/service/api/problem.ts
+++ src/service/api/problem.ts
@@ -1,16 +1,29 @@
-import { END_VALUE, START_VALUE } from '@/constant'
 import { SolvedAcProblem } from '@/types'
 
 import { solvedAcApi } from '../instance'
+
+type SearchProblemRequestParams = {
+  value: number
+  page: number
+}
 
 type SearchProblemResponse = {
   count: number
   items: SolvedAcProblem[]
 }
 
-export const levelProblemApi = async () => {
+export const levelProblemApi = async ({
+  value,
+  page,
+}: SearchProblemRequestParams) => {
   const response = await solvedAcApi.get<SearchProblemResponse>(
-    `/api/v3/search/problem?query=(*${START_VALUE}..${END_VALUE})`,
+    '/api/v3/search/problem',
+    {
+      params: {
+        query: `(*${value}..${value})`,
+        page,
+      },
+    },
   )
 
   return response.data.items
