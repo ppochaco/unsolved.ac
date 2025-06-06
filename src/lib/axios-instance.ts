680f16b300cb9ae8270fa701ce2562c14f3ef258
ajin <aj5210@naver.com>
2025-06-06 18:41:30 +0900

    Remove: axios instance 기본 타임아웃 시간 제거
    - solved.ac API 요청이 30s 정도 소요됨

--- src/lib/axios-instance.ts
+++ src/lib/axios-instance.ts
@@ -1,10 +1,7 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
 
-const TIMEOUT = 2000
-
 export const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
   const instance = axios.create({
-    timeout: TIMEOUT,
     ...config,
     headers: {
       Accept: 'application/json',
