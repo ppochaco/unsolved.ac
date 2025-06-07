cc151dc3ad97d0d409220aff51eba2a53e1e7a20
ajin <aj5210@naver.com>
2025-06-07 15:40:03 +0900

    Feat: Header 컴포넌트 추가

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,3 +1,19 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export const Header = () => {
-  return <div className="h-18 w-full bg-amber-50">header</div>
+  return (
+    <header className="flex h-18 w-full items-center px-4">
+      <Link href="/" className="flex items-center gap-1.5">
+        <Image
+          src="/favicon.ico"
+          width={32}
+          height={32}
+          alt="unsolve.ac logo"
+          className="object-contain"
+        />
+        <div className="font-roboto text-md">unsolved.ac</div>
+      </Link>
+    </header>
+  )
 }
