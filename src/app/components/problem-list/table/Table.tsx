c2d9582fc5eb7deaac07ccaf259440ad3e8178f4
ajin <aj5210@naver.com>
2025-06-07 16:51:17 +0900

    Feat: ProblemListTable 컴포넌트 추가

---
+++ src/app/components/problem-list/table/Table.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+
+import { Level, Problem } from '@/generated/prisma'
+
+interface ProblemListTableProps {
+  columns: { header: string; accessor: string }[]
+  problems: Problem[]
+  levels: Level[]
+}
+
+export const ProblemListTable = ({
+  columns,
+  problems,
+  levels,
+}: ProblemListTableProps) => {
+  const levelImages = new Map(levels.map((l) => [l.id, l.imageUrl]))
+
+  return (
+    <table className="w-full">
+      <thead>
+        <tr>
+          {columns.map((column) => (
+            <th key={column.accessor} className="border-b py-2.5 font-semibold">
+              {column.header}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {problems.map((problem) => (
+          <tr key={problem.id} className="w-full border-b">
+            <td className="w-10 py-2.5">
+              <div className="flex items-center justify-center">
+                <Image
+                  src={levelImages.get(problem.levelId) ?? ''}
+                  width={16}
+                  height={16}
+                  className="object-contain"
+                  alt={`solved.ac ${problem.levelId} tier`}
+                />
+              </div>
+            </td>
+            <td className="w-24 py-2.5">{problem.id}</td>
+            <td className="py-2.5">{problem.title}</td>
+            <td className="w-20 py-2.5 text-end">{problem.solvedCount}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
