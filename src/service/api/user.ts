be74ecf6b5bd7d58e81e5c139c3e8a40f00a3f03
ajin <aj5210@naver.com>
2025-06-09 00:42:35 +0900

    Rename: /api/user을 /api/users로 수정

--- src/service/api/user.ts
+++ src/service/api/user.ts
@@ -2,12 +2,10 @@ import axios from 'axios'
 
 import { SolvedAcUser } from '@/types'
 
-export const fetchUserInfoApi = async (
-  userId: string,
-): Promise<SolvedAcUser> => {
-  const { data } = await axios.get<SolvedAcUser>('/api/user', {
+export const fetchUserInfoApi = async (userId: string) => {
+  const response = await axios.get<SolvedAcUser>('/api/users', {
     params: { userId },
   })
 
-  return data
+  return response.data
 }
