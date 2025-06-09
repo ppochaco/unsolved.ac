3e28588c841f056f5e7b4dae144c04fb6c1b8765
ajin <aj5210@naver.com>
2025-06-09 16:01:43 +0900

    Design: TagSelect 문제 수 우측 정렬

--- src/app/components/problem-filter/tag-select/TagSelect.tsx
+++ src/app/components/problem-filter/tag-select/TagSelect.tsx
@@ -17,16 +17,18 @@ interface TagSelectProps {
 export const TagSelect = ({ tags, value, selectTag }: TagSelectProps) => {
   return (
     <Select value={value || ''} onValueChange={selectTag}>
-      <SelectTrigger className="w-full">
+      <SelectTrigger className="relative w-full flex-1">
         <SelectValue placeholder="태그를 선택하세요" />
       </SelectTrigger>
       <SelectContent>
-        <SelectGroup>
+        <SelectGroup className="max-w-min">
           {tags.map((tag) => (
-            <SelectItem key={tag.id} value={tag.key} className="flex w-full">
-              <div>{tag.name}</div>
-              <div className="text-plum-400">#{tag.key}</div>
-              <div>{tag.problemCount}</div>
+            <SelectItem key={tag.id} value={tag.key} className="relative pr-16">
+              <div className="flex max-w-52 flex-col items-start">
+                <div>{tag.name}</div>
+                <div className="text-plum-400">#{tag.key}</div>
+              </div>
+              <div className="absolute right-8 text-sm">{tag.problemCount}</div>
             </SelectItem>
           ))}
         </SelectGroup>
