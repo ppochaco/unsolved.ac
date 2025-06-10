e7b7bb97ba7a6e0659140351d02b0921382f7c5e
ajin <aj5210@naver.com>
2025-06-10 22:48:41 +0900

    Refactor: 상대경로 api를 절대경로로 수정
    - SSR 환경에서는 상대경로 처리 불가

--- src/service/instance.ts
+++ src/service/instance.ts
@@ -3,3 +3,10 @@ import { initInstance } from '@/lib'
 export const solvedAcApi = initInstance({
   baseURL: 'https://solved.ac',
 })
+
+export const unsolvedApi = initInstance({
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'https://unsolved-ac.com',
+})
