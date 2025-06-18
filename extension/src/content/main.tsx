4351fa63d4c25470f990797875c300f9506db7b2
ajin <aj5210@naver.com>
2025-06-18 15:22:50 +0900

    Feat: shadow DOM 생성, 제거 기능 추가

---
+++ extension/src/content/main.tsx
@@ -0,0 +1,22 @@
+import { createRoot } from 'react-dom/client'
+
+import tailwindStyles from '../index.css?inline'
+import { UserFilter } from './UserFilter'
+
+export const createShadowDOM = (
+  shadowRoot: ShadowRoot,
+  onClose: () => void,
+) => {
+  const reactContainer = document.createElement('div')
+  shadowRoot.appendChild(reactContainer)
+
+  const styleElement = document.createElement('style')
+  styleElement.textContent = tailwindStyles
+  shadowRoot.appendChild(styleElement)
+
+  const root = createRoot(reactContainer)
+
+  root.render(<UserFilter onClose={onClose} />)
+
+  return root
+}
