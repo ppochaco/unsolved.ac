41a1419a029deafbcf2c6fc245beebf8d7c6db77
ajin <aj5210@naver.com>
2025-06-19 23:40:22 +0900

    Rename: constant default-image-url을 solved-ac-url로 네이밍 변경

--- extension/src/constants/default-image-url.tsx
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_USER_IMAGE_URL =
-  'https://static.solved.ac/misc/360x360/default_profile.png'
--- extension/src/constants/index.ts
+++ extension/src/constants/index.ts
@@ -1,2 +1,2 @@
 export { ERROR_MESSAGES } from './error-message'
-export { DEFAULT_USER_IMAGE_URL } from './default-image-url'
+export { DEFAULT_USER_IMAGE_URL, SOLVED_AC_PROBLEMS_URL } from './solved-ac-url'
