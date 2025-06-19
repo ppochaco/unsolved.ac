3047cf8a2a9a775d127619555150af808c6f4a2f
ajin <aj5210@naver.com>
2025-06-19 16:59:01 +0900

    Chore: shadcn Card, Input 컴포넌트 추가

--- extension/src/components/ui/index.ts
+++ extension/src/components/ui/index.ts
@@ -1,5 +1,15 @@
 export { buttonVariants } from './button-variants'
 export { Button } from './button'
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
+} from './card'
+export { Input } from './input'
 export { Label } from './label'
 export { Spinner } from './spinner'
 export { Switch } from './switch'
